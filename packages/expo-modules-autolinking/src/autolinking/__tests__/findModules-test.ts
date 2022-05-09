ec69eca3939991b4458b8a1bc06eee1b56c2b9a3
Kudo Chien <kudo@expo.dev>
2022-05-09 23:30:01 +0800

    [autolinking] add some unit tests for findModules in workspace

--- packages/expo-modules-autolinking/src/autolinking/__tests__/findModules-test.ts
+++ packages/expo-modules-autolinking/src/autolinking/__tests__/findModules-test.ts
@@ -14,6 +14,17 @@ jest.mock('fs-extra');
 
 // mock findUp.sync to fix `mergeLinkingOptions` package.json resolution when requiring `findModules`.
 (findUp.sync as jest.MockedFunction<any>).mockReturnValueOnce(path.join(expoRoot, 'package.json'));
+const mockProjectPackageJsonPath = jest.fn();
+jest.mock('../mergeLinkingOptions', () => {
+  const actualModule = jest.requireActual('../mergeLinkingOptions');
+  return {
+    ...actualModule,
+    get projectPackageJsonPath() {
+      return mockProjectPackageJsonPath();
+    },
+  };
+});
+
 const {
   findModulesAsync,
 }: { findModulesAsync: typeof findModulesAsyncType } = require('../findModules');
@@ -27,17 +38,17 @@ describe(findModulesAsync, () => {
     jest.resetAllMocks();
   });
 
-  function addMockedModule(name: string) {
-    const pkgDir = path.join('node_modules', name);
+  function addMockedModule(name: string, options?: { pkgDir?: string; pkgVersion?: string }) {
+    const pkgDir = options?.pkgDir ?? path.join(expoRoot, 'node_modules', name);
 
     // mock require() call to module's package.json
-    registerRequireMock(path.join(expoRoot, pkgDir, 'package.json'), {
+    registerRequireMock(path.join(pkgDir, 'package.json'), {
       name,
-      version: '0.0.1',
+      version: options?.pkgVersion ?? '0.0.1',
     });
 
     // mock require() call to module's expo-module.config.json
-    registerRequireMock(path.join(expoRoot, pkgDir, 'expo-module.config.json'), {
+    registerRequireMock(path.join(pkgDir, 'expo-module.config.json'), {
       platforms: ['ios'],
     });
   }
@@ -52,7 +63,7 @@ describe(findModulesAsync, () => {
       searchPaths: [searchPath],
       platform: 'ios',
     });
-    expect(result['react-native-third-party']).not.toBeNull();
+    expect(result['react-native-third-party']).not.toBeUndefined();
   });
 
   it('should link scoped level package', async () => {
@@ -73,5 +84,64 @@ describe(findModulesAsync, () => {
       platform: 'ios',
     });
     expect(Object.keys(result).length).toBe(2);
+  });
+
+  it('should link hoisted package in workspace', async () => {
+    const appPackageJsonPath = path.join(expoRoot, 'packages', 'app', 'package.json');
+    const appNodeModules = path.join(expoRoot, 'packages', 'app', 'node_modules');
+
+    // mock app project package.json
+    mockProjectPackageJsonPath.mockReturnValue(appPackageJsonPath);
+    registerRequireMock(appPackageJsonPath, {
+      name: 'app',
+      version: '0.0.1',
+      dependencies: {
+        pkg: '*',
+      },
+    });
+
+    // add mocked pkg
+    const workspaceNodeModules = path.join(expoRoot, 'node_modules');
+    const searchPaths = [appNodeModules, workspaceNodeModules];
+    addMockedModule('pkg', { pkgDir: path.join(workspaceNodeModules, 'pkg') });
+    registerGlobMock(glob, ['pkg/expo-module.config.json'], workspaceNodeModules);
+
+    const result = await findModulesAsync({
+      searchPaths,
+      platform: 'ios',
+    });
+
+    expect(result['pkg']).not.toBeUndefined();
+  });
+
+  it('should link non-hoisted package from app project dependencies', async () => {
+    const appPackageJsonPath = path.join(expoRoot, 'packages', 'app', 'package.json');
+    const appNodeModules = path.join(expoRoot, 'packages', 'app', 'node_modules');
+
+    // mock app project package.json
+    mockProjectPackageJsonPath.mockReturnValue(appPackageJsonPath);
+    registerRequireMock(appPackageJsonPath, {
+      name: 'app',
+      version: '0.0.1',
+      dependencies: {
+        pkg: '~1.0.0',
+      },
+    });
+
+    // add mocked pkgs
+    const workspaceNodeModules = path.join(expoRoot, 'node_modules');
+    const searchPaths = [appNodeModules, workspaceNodeModules];
+    addMockedModule('pkg', { pkgDir: path.join(workspaceNodeModules, 'pkg'), pkgVersion: '0.0.0' });
+    addMockedModule('pkg', { pkgDir: path.join(appNodeModules, 'pkg'), pkgVersion: '1.0.0' });
+    registerGlobMock(glob, ['pkg/expo-module.config.json'], workspaceNodeModules);
+    registerGlobMock(glob, ['pkg/expo-module.config.json'], appNodeModules);
+
+    const result = await findModulesAsync({
+      searchPaths,
+      platform: 'ios',
+    });
+
+    expect(result['pkg']).not.toBeUndefined();
+    expect(result['pkg'].version).toEqual('1.0.0');
   });
 });
